4fded8066553735df27aff5decc44a4ac06837cb
cfdj <callumderry@gmail.com>
2018-07-02 11:48:27 +0100

    Player movement and camera control added
    Not fully working, feels a bit weird

---
+++ MeandMartin/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float moveSpeed;
+	private Rigidbody rb;
+	private float xaxis;
+	private float zaxis;
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Fixed Update used for player movement and physics
+	void FixedUpdate () {
+		xaxis = Input.GetAxis ("Horizontal");
+		zaxis = Input.GetAxis ("Vertical");
+		rb.velocity = new Vector3 (xaxis * moveSpeed, rb.velocity.y, zaxis * moveSpeed);
+	}
+}
