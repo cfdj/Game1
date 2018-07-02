eb1c26d6b88b7f0487fddc0c46fa521555d450fb
cfdj <callumderry@gmail.com>
2018-07-02 17:24:11 +0100

    turning now works better
    Player controls improved, however turning still feels off, might be lack
    of animation or just too fast

--- MeandMartin/Assets/Scripts/PlayerController.cs
+++ MeandMartin/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 	public float moveSpeed;
+	public float sprintSpeed;
+	private float sprintingMul;
+	public float turnSpeed;
 	private Rigidbody rb;
 	private float xaxis;
 	private float zaxis;
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
+		sprintingMul = 3.0f;
+		sprintSpeed = 1.0f;
+	}
+	void Update(){
+		if (Input.GetKey ("space")) {
+			sprintSpeed = sprintingMul;
+		} else {
+			sprintSpeed = 1.0f;
+		}
 	}
 	
 	// Fixed Update used for player movement and physics
 	void FixedUpdate () {
-		xaxis = Input.GetAxis ("Horizontal");
-		zaxis = Input.GetAxis ("Vertical");
-		rb.velocity = new Vector3 (xaxis * moveSpeed, rb.velocity.y, zaxis * moveSpeed);
+		zaxis = Input.GetAxis ("Horizontal");
+		xaxis = Input.GetAxis ("Vertical");
+		rb.transform.Rotate (0, zaxis * turnSpeed, 0);
+		//rb.rotation = Quaternion.Euler (rb.rotation.x,rb.rotation.y + (zaxis * turnSpeed), rb.rotation.z);
+		rb.velocity = transform.forward * xaxis * moveSpeed * sprintSpeed;
 	}
 }
