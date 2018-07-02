da21ea1bc1b33a4169b2342c9726ad475405e7e4
cfdj <callumderry@gmail.com>
2018-07-02 19:56:41 +0100

    Dash and Spriting improved

--- MeandMartin/Assets/Scripts/PlayerController.cs
+++ MeandMartin/Assets/Scripts/PlayerController.cs
@@ -7,9 +7,14 @@ public class PlayerController : MonoBehaviour {
 	public float sprintSpeed;
 	private float sprintingMul;
 	public float turnSpeed;
+	private float currentTurnSpeed;
 	private Rigidbody rb;
 	private float xaxis;
 	private float zaxis;
+	private bool dashing;
+	private float dashEnd;
+	public float dashLength;
+	public float dashSpeed;
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody> ();
@@ -17,19 +22,36 @@ public class PlayerController : MonoBehaviour {
 		sprintSpeed = 1.0f;
 	}
 	void Update(){
-		if (Input.GetKey ("space")) {
-			sprintSpeed = sprintingMul;
-		} else {
-			sprintSpeed = 1.0f;
+		if ((Input.GetKey ("q") && (dashing == false))) {
+			dashEnd = Time.time + dashLength;
+			dashing = true;
+			sprintSpeed = dashSpeed;
 		}
+		if (Time.time >= dashEnd) {
+			sprintSpeed = 1.0f;
+			dashing = false;
+			if (Input.GetKey ("space")) {
+				sprintSpeed = sprintingMul;
+			} else {
+				sprintSpeed = 1.0f;
+			}
+		}
+
+
 	}
 	
 	// Fixed Update used for player movement and physics
 	void FixedUpdate () {
 		zaxis = Input.GetAxis ("Horizontal");
 		xaxis = Input.GetAxis ("Vertical");
-		rb.transform.Rotate (0, zaxis * turnSpeed, 0);
+		if (dashing == true) {
+			currentTurnSpeed = 0;
+		} else {
+			currentTurnSpeed = turnSpeed;
+
+		}
+		rb.transform.Rotate (0, zaxis * currentTurnSpeed, 0);
 		//rb.rotation = Quaternion.Euler (rb.rotation.x,rb.rotation.y + (zaxis * turnSpeed), rb.rotation.z);
-		rb.velocity = transform.forward * xaxis * moveSpeed * sprintSpeed;
+		rb.velocity = transform.forward * xaxis * moveSpeed * sprintSpeed; //this's location here does mean the player can change direction during their dash
 	}
 }
